8961ce13db1d7c25bb018e26a95a1bdccb566260
SurelySomeday <648951430@qq.com>
2022-11-10 10:35:34 +0800

    完善grpc-server注册

---
+++ grpc-server/src/main/java/com/example/GrpcNacosRegistration.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import com.alibaba.cloud.nacos.registry.NacosRegistration;
+import com.alibaba.cloud.nacos.registry.NacosServiceRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author yanxin.
+ * @Date 2022/11/10 9:47.
+ * Created by IntelliJ IDEA
+ * File Description:
+ */
+@Component
+public class GrpcNacosRegistration implements CommandLineRunner {
+
+    @Value("${grpc.port:6565}")
+    Integer port;
+    @Resource
+    NacosRegistration nacosRegistration;
+    @Resource
+    NacosServiceRegistry nacosServiceRegistry;
+
+    @Override
+    public void run(String... args) {
+        nacosRegistration.setPort(port);
+        nacosServiceRegistry.register(nacosRegistration);
+    }
+}
